3117eef3b8ae1596f55970bd7e364c1ccbed4b44
DaHeeO <134845297+DaHeeO@users.noreply.github.com>
2024-05-05 19:29:01 +0900

    Project

---
+++ src/main/kotlin/dahee/project/spring/kotlin/Application.kt
@@ -0,0 +1,11 @@
+package dahee.project.spring.kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringKotlinApplication
+
+fun main(args: Array<String>) {
+  runApplication<SpringKotlinApplication>(*args)
+}
